0a4f9554d87d56a301ef266a2795aed865e72443
Federico Gomez <fedegomezdev@gmail.com>
2020-02-16 21:17:09 -0300

    better ui

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -11,8 +11,9 @@ export default function HomeScreen({navigation}:any) {
          
         <TouchableOpacity onPress={() => navigation.navigate('Drinks')}>
         <Text style={style.titulo}>COCKTAIL </Text>
-        <Image source={require('../assets/icons/bebidas.png')} style={{width:200, height:200, alignSelf:"center"}}/>
         <Text style={style.texto}>Find your Drink </Text>
+        <Image source={require('../assets/icons/cocktailicon.png')} style={style.image}/>
+        <Text style={style.search}>Search</Text>
         </TouchableOpacity>
   
       </View>
@@ -22,29 +23,42 @@ export default function HomeScreen({navigation}:any) {
 const style = StyleSheet.create({
   container: {
       flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center' ,
+      alignItems: 'center',    
       backgroundColor:'#f1b950',
       borderBottomLeftRadius:100,
       borderTopRightRadius:100,
-     
   },
   texto :{
       fontSize:20,
       fontWeight:'bold',
-      marginBottom:15,
-      alignSelf:'center',
-      marginTop:25,
+      marginBottom:100,
+      
+      marginTop:10,
       color:'#201b17',
-      borderBottomColor:'#201b17',
-      borderBottomWidth:3
+
   },
   titulo : {
-    fontSize:30,
+    fontSize:60,
     fontWeight:'bold',
-    marginBottom:15,
-    alignSelf:'center',
-    marginTop:15,
-    color:'#201b17'
+    
+    marginTop:35,
+    color:'#201b17',
+    borderBottomColor:'#201b17',
+    borderBottomWidth:3
+  },
+  search: {
+    backgroundColor: 'white',
+    color: '#201b17',
+    borderRadius: 25,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize:  27,
+    marginTop:150,
+    paddingVertical:20
+  },
+  image: {
+    width:200,
+    height:200,
+    alignSelf:"center"
   }
 })
